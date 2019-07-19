7c03d0ca05773c2048e1e3d1e7645e4d3d1a1826
nealith <nealith@neaworld.fr>
2019-07-20 01:58:51 +0200

    improve curves

--- road/plane.gd
+++ road/plane.gd
@@ -5,6 +5,7 @@ extends Polygon2D
 # var b = "text"
 
 var dz = 0
+var t = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +14,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	dz+= delta*1000
+	t += delta
 	if dz > 540.0:
 		dz -= 540.0
 	self.material.set_shader_param("delta",Vector3(0.0,0.0,dz))
+	
+	var v = sin(t)
+	
+	#self.material.set_shader_param("X_y_a",Vector2(0.0,v))
+	#self.material.set_shader_param("X_y_b",Vector2(-v/2.0,-4*v))
 	
